f60dd535520d434824b39304b0ac2100d0ae4c8c
Victoria-27 <victoria.abioye@decagon.dev>
2022-02-09 12:16:58 +0100

    first commit

---
+++ generator/__test__/sometest.test.js
@@ -0,0 +1,11 @@
+// const supertest = require("supertest")
+// const app = require("../app")
+
+// const server = supertest(app)
+
+// describe("a basic test", (done)=>{
+//     it("should test", (err, next)=>{
+//         server
+
+//     })
+// })
